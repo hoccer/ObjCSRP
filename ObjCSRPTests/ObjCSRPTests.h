09bd754ceb720bb0658950e8a3890b5fd0a75b2e
David Siegel <david.siegel@artcom.de>
2013-04-02 23:00:57 +0200

    Initial Commit

---
+++ ObjCSRPTests/ObjCSRPTests.h
@@ -0,0 +1,13 @@
+//
+//  ObjCSRPTests.h
+//  ObjCSRPTests
+//
+//  Created by David Siegel on 02.04.13.
+//  Copyright (c) 2013 Hoccer GmbH. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ObjCSRPTests : SenTestCase
+
+@end
---
+++ ObjCSRPTests/ObjCSRPTests.m
@@ -0,0 +1,32 @@
+//
+//  ObjCSRPTests.m
+//  ObjCSRPTests
+//
+//  Created by David Siegel on 02.04.13.
+//  Copyright (c) 2013 Hoccer GmbH. All rights reserved.
+//
+
+#import "ObjCSRPTests.h"
+
+@implementation ObjCSRPTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ObjCSRPTests");
+}
+
+@end
